6438fd460ed7990ab400772d9f026c86f078632a
chaitos <advsanture@gmail.com>
2025-09-04 07:41:44 +0600

    комит проекта для деплоя на рендер, изменен Dockerfile и settings.py

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,23 @@
 FROM python:3.12-slim
 
+# рабочая папка
 WORKDIR /app
 
+# зависимости
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt gunicorn
 
+# код
 COPY . .
 
+# рабочая папка проекта
 WORKDIR /app/QAService
 
+# собираем статику
+RUN python manage.py collectstatic --noinput
+
+# порт
 EXPOSE 8000
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+
+# запуск через gunicorn
+CMD ["gunicorn", "QAService.wsgi:application", "--bind", "0.0.0.0:8000"]
--- QAService/QAService/settings.py
+++ QAService/QAService/settings.py
@@ -1,12 +1,18 @@
-
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DEBUG = False  # в продакшене
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+ALLOWED_HOSTS = ["*"]  # Render сам подставит домен
+
+# STATIC
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f4l-zlw--%mvy913n&z8aqezj*lnx(s_sr^r(bjx2vwjruldm)'
@@ -59,8 +65,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'QAService.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -69,9 +73,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -89,8 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru-Ru'
 
@@ -101,13 +100,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
